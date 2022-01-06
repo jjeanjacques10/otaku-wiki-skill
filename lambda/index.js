cc56bd359d76d11b5f2b9055d800ccbf9642d374
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-01-06 00:39:47 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -56,7 +56,7 @@ const AnimeIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            .reprompt(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
@@ -82,7 +82,7 @@ const MangaIntentHandler = {
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
-            .reprompt(speakOutput)
+             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
